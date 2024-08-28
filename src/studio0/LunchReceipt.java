b8abb2464490e2a339773f76249159e1aa222e20
annazimmermann <annazimmermann@localhost>
2024-08-28 13:56:43 -0500

    I made changes

--- src/studio0/LunchReceipt.java
+++ src/studio0/LunchReceipt.java
@@ -1,21 +1,18 @@
 package studio0;
 
-public class LunchReceipt {
-
+public class MileageReport {
+	
 	public static void main(String[] args) {
+		int car1miles = 250;
+		int car2miles = 100;
+		int car3miles = 900;
 		
-		double burritoPrice = 7.5;
-		double drinkPrice = 1.25;
-		double chipsPrice = 1.00;
-		double guacPrice = 2.50;
+		int totalMiles = car1miles + car2miles + car3miles;
+		double milesPerGallon = 24.5;
+		double totalGallons = totalMiles * milesPerGallon;
+		double pricePerGallon = 3.05;
+		double totalPrice = totalGallons * pricePerGallon;
 		
-		double subtotal = burritoPrice + drinkPrice + chipsPrice + guacPrice;
-		double taxRate = 0.04225;
-		double taxAmount = subtotal * taxRate;
-		double total = subtotal + taxAmount;
-		
-		System.out.println("Your total was: $" + total);
+		System.out.println("Total price spent on gas was $" + totalPrice);
 	}
-	
-	
 }
